e628d0e8ecc3f1713b63f34bdf65ae4687cf2c69
Dawid Janiga <dawidjaniga@Dawids-MacBook-Pro.local>
2022-11-01 14:55:23 +0100

    init

---
+++ pages/learn.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Index() {
+    return (
+        <div >
+            <Head>
+                <title>Learn</title>
+                <meta name="description" content="Learn" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Learn</h1>
+        </div>
+
+    )
+}
