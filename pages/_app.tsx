a29f9f48b4d4d2eb390c64f65b95fa629ecc6a4f
Dawid Janiga <dawidjaniga@gmail.com>
2022-11-02 12:12:11 +0100

    feat(theme): change success color to green

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
-import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import { CssBaseline, GeistProvider, Themes } from '@geist-ui/core'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+const myTheme1 = Themes.createFromDark({
+  type: 'coolTheme',
+  palette: {
+    success: '#4FBF26',
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GeistProvider themeType='dark'>
+    <GeistProvider themes={[myTheme1]} themeType='coolTheme'>
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
